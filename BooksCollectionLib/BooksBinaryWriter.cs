340bfa24bf444f028c7980537a2ea6b469b77bc5
maximgorbachiov <maksimka.gorbachev.95.gmail.com>
2016-03-24 05:01:32 +0300

    Задание 5-го дня

---
+++ BooksCollectionLib/BooksBinaryWriter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksCollectionLib
+{
+    public class BooksBinaryWriter<T> : IBookStream where T : Book, new()
+    {
+        public Book[] ReadBooks(string filePath)
+        {
+            List<Book> books = new List<Book>();
+            int countOfPages;
+
+            if ((filePath != "") && (filePath != null))
+            {
+                using (BinaryReader reader = new BinaryReader(new FileStream(filePath, FileMode.Open)))
+                {
+                    while (reader.PeekChar() > -1)
+                    {
+                        Book book = new T();
+                        book.Title = reader.ReadString();
+                        book.Author = reader.ReadString();
+                        int.TryParse(reader.ReadString(), out countOfPages);
+                        book.CountOfPages = countOfPages;
+                        books.Add(book);
+                    }
+                }
+
+                return books.ToArray();
+            }
+            throw new ArgumentNullException(nameof(filePath));
+        }
+
+        public void WriteBooks(Book[] books, string filePath)
+        {
+            if ((filePath == "") && (filePath == null))
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            using (BinaryWriter writer = new BinaryWriter(new FileStream(filePath, FileMode.OpenOrCreate)))
+            {
+                for (int i = 0; i < books.Length; i++)
+                {
+                    if (books[i] != null)
+                    {
+                        writer.Write(books[i].Title);
+                        writer.Write(books[i].Author);
+                        writer.Write(books[i].CountOfPages.ToString());
+                    }
+                }
+            }           
+        }
+    }
+}
---
+++ TestBooksCollection/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooksCollectionLib;
+
+namespace TestBooksCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BooksCollection books = new BooksCollection(new BooksBinaryWriter<UserBook>());
+
+            books.AddBook(new UserBook("Harry Potter and the Philosopher's Stone", "J. Rolling", 200));
+            books.AddBook(new UserBook("Harry Potter and the Chamber of Secrets", "J. Rolling", 300));
+            books.AddBook(new UserBook("Harry Potter and the Prisoner of Azkaban", "J. Rolling", 400));
+            books.AddBook(new UserBook("Harry Potter and the Goblet of Fire", "J. Rolling", 500));
+            books.AddBook(new UserBook("Harry Potter and the Order of the Phoenix", "J. Rolling", 800));
+            books.AddBook(new UserBook("Harry Potter and the Half-Blood Prince", "J. Rolling", 500));
+            books.AddBook(new UserBook("Harry Potter and the Deathly Hallows", "J. Rolling", 600));
+
+            Console.WriteLine("Test ADD");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < books.Count; i++)
+            {
+                Console.WriteLine(books[i].ToString());
+            }
+
+            books.RemoveBook(books[books.Count - 1]);
+            books.RemoveBook(books[books.Count - 1]);
+            books.RemoveBook(books[books.Count - 1]);
+
+            Console.WriteLine("Test DELETE");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < books.Count; i++)
+            {
+                Console.WriteLine(books[i].ToString());
+            }
+
+            books.AddBook(new UserBook("Harry Potter and the Order of the Phoenix", "J. Rolling", 800));
+            books.AddBook(new UserBook("Harry Potter and the Half-Blood Prince", "J. Rolling", 500));
+            books.AddBook(new UserBook("Harry Potter and the Deathly Hallows", "J. Rolling", 600));
+
+            IEnumerable<Book> repeatingElements = books.FindByTag(new UserBook("Harry Potter and the Half-Blood Prince", "J. Rolling", 500));
+
+            Console.WriteLine("Test FIND_BY_TAG");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (Book book in repeatingElements)
+            {
+                Console.WriteLine(book.ToString());
+            }
+
+            books.SortBooksByTag(new Comparator());
+
+            Console.WriteLine("Test SORT_BY_TAG");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < books.Count; i++)
+            {
+                Console.WriteLine(books[i].ToString());
+            }
+
+            books.WriteToStream(@"D:\temp.txt");
+
+            books.ReadFromStream(@"D:\temp.txt");
+
+            Console.WriteLine("Test READ_FROM_STREAM");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < books.Count; i++)
+            {
+                Console.WriteLine(books[i].ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
